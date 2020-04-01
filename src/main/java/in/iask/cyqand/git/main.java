0c32456873e03dd0927745f4006bf1fd48b2bb4c
cyqand_pro <cyqand_pro@git.com>
2020-04-01 16:19:42 +0800

    test branch hot_fix

--- src/main/java/in/iask/cyqand/git/main.java
+++ src/main/java/in/iask/cyqand/git/main.java
@@ -4,5 +4,6 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("TestGit");
 		System.out.println("eclipse_java_git_TestGit2");
+		System.out.println("hot_fix");
 	}
 }
